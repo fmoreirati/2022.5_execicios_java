7e5890c2c0b377bc838f73c41957be6875ddf953
Fabiano Moreira <fabianomoreira.ti@gmail.com>
2023-03-15 20:36:00 -0300

    Aula03

--- src/exercicios/Numero.java
+++ src/exercicios/Numero.java
@@ -2,31 +2,68 @@ package exercicios;
 
 public class Numero {
     int valor;
-    
-    public String parImpar(){
-        if(this.valor % 2 ==0 ){
+
+    public String parImpar() {
+        if (this.valor % 2 == 0) {
             return "PAR";
-        }else {
+        } else {
             return "IMPAR";
         }
     }
 
-    public String parImpar(int valor){
-        return "";
+    public String parImpar(int valor) {
+        if (valor % 2 == 0) {
+            return "PAR";
+        } else {
+            return "IMPAR";
+        }
     }
 
-// 7) Escreva um programa para ler 2 valores (considere que não serão informados valores iguais) e escrever o maior deles. 
-    public int maiorValor(int valor1, int valor2){
-        if(valor1 > valor2){
+    // 7) Escreva um programa para ler 2 valores (considere que não serão informados valores iguais) e escrever o maior deles.
+    public int maiorValor(int valor1, int valor2) {
+        if (valor1 > valor2) {
             return valor1;
         }
         return valor2;
     }
 
-    public String ordenarCrescente(int valor1, int valor2, int valor3){
-        return "";
+    // 10) Escreva um programa para ler 3 valores inteiros (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
+    public String ordenarCrescente(int valor1, int valor2, int valor3) {
+        // if(valor1 < valor2 && valor2 < valor3){
+        // return valor1 + " - " + valor2 + " - " + valor3;
+        // }
+        // if(valor1 < valor3 && valor3 < valor2){
+        // return valor1 + " - " + valor3 + " - " + valor2;
+        // }
+        // if(valor2 < valor1 && valor1 < valor3){
+        // return valor2 + " - " + valor1 + " - " + valor3;
+        // }
+        // if(valor2 < valor3 && valor3 < valor1){
+        // return valor2 + " - " + valor3 + " - " + valor1;
+        // }
+        // if(valor3 < valor1 && valor1 < valor2){
+        // return valor3 + " - " + valor1 + " - " + valor2;
+        // }
+        // return valor3 + " - " + valor2 + " - " + valor1;
+
+        int temp = 0;
+        if (valor1 > valor2) {
+            temp = valor1;
+            valor1 = valor2;
+            valor2 = temp;
+        }
+
+        if (valor2 > valor3) {
+            temp = valor3;
+            valor3 = valor2;
+            valor2 = temp;
+        }
+
+        if (valor1 > valor3) {
+            temp = valor3;
+            valor3 = valor1;
+            valor1 = temp;
+        }
+        return valor1 + " - " + valor2 + " - " + valor3;
     }
 }
-
-
-
--- src/exercicios/Principal.java
+++ src/exercicios/Principal.java
@@ -4,21 +4,18 @@
 package exercicios;
 
 public class Principal {
-
     public static void main(String[] args) {
-//        int valor = 3;
-//        
+//        int valor = 3;        
 //        if (valor % 2 == 0){
 //            System.out.print("PAR");
 //        }else{
 //            System.out.print("IMPAR");
 //        };
-
             Numero numero = new Numero();
             numero.valor =4;
-            System.out.print(numero.parImpar());
-
-            System.out.print(numero.maiorValor(6,8));
+            System.out.println(numero.parImpar());
+            System.out.println(numero.maiorValor(6,8));
+            System.out.println(numero.ordenarCrescente(94,5,39));           
     }
     
 }
