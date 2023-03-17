8483c84831fda0a27086574773857e6edfb994cc
Fabiano Moreira <fabianomoreira.ti@gmail.com>
2023-03-17 19:46:28 -0300

    Aula 04

--- src/exercicios/bola/Bola.java
+++ src/exercicios/bola/Bola.java
@@ -1,5 +1,15 @@
 package exercicios.bola;
 
 public class Bola {
+    private String cor;
+    private double circunferencia; // 10.5
+    private String material;
 
+    public void trocaCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String mostrarCor() {
+        return cor;
+    }
 }
--- src/exercicios/bola/Principal.java
+++ src/exercicios/bola/Principal.java
@@ -1,6 +1,17 @@
 package exercicios.bola;
 
+import javax.swing.JOptionPane;
+
 public class Principal {
     public static void main(String[] args) {
+        Bola bolinha = new Bola();
+        bolinha.trocaCor("vermelho");
+        bolinha.circunferencia = 10.5;
+        bolinha.material = "metal";
+
+        JOptionPane.showMessageDialog(null, ""
+                + "Cor: " + bolinha.mostrarCor() + "\n"
+                + "Circunferência: " + bolinha.circunferencia + "\n"
+                + "Material: " + bolinha.material + "\n");
     }
 }
