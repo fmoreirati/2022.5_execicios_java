b3b6fa49a26a2b9404cc7ebba8b00c63e4a86704
Fabiano Moreira <fabianomoreira.ti@gmail.com>
2023-03-17 21:39:48 -0300

    Aula 04

--- src/exercicios/bola/Principal.java
+++ src/exercicios/bola/Principal.java
@@ -4,17 +4,28 @@ import javax.swing.JOptionPane;
 
 public class Principal {
     public static void main(String[] args) {
-        Bola bolinha = new Bola();
 
-        bolinha.trocarCor(JOptionPane.showInputDialog("Qual a cor?"));
-
-        bolinha.trocarCircunferencia(Double.parseDouble(JOptionPane.showInputDialog("Qual a circunferência?")));
-
-        bolinha.trocarMaterial(JOptionPane.showInputDialog("Qual a material?"));
-
-        JOptionPane.showMessageDialog(null, ""
-                + "Cor: " + bolinha.mostrarCor() + "\n"
-                + "Circunferência: " + bolinha.mostrarCircunferencia() + "\n"
-                + "Material: " + bolinha.mostrarMaterial() + "\n");
+        try {
+            String novaCor = " ";
+            while (!novaCor.equals("")) {
+                // Entrada dados
+                novaCor = JOptionPane.showInputDialog("Qual a cor?");
+                if (!novaCor.equals("")) {
+                    // Criação Objeto
+                    Bola bolinha = new Bola();
+                    bolinha.trocarCor(novaCor);
+                    bolinha.trocarCircunferencia(
+                            Double.parseDouble(JOptionPane.showInputDialog("Qual a circunferência?")));
+                    bolinha.trocarMaterial(JOptionPane.showInputDialog("Qual a material?"));
+                    // Saida dados
+                    JOptionPane.showMessageDialog(null, ""
+                            + "Cor: " + bolinha.mostrarCor() + "\n"
+                            + "Circunferência: " + bolinha.mostrarCircunferencia() + "\n"
+                            + "Material: " + bolinha.mostrarMaterial() + "\n");
+                }
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
     }
 }
